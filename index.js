3b230b77ff4c54b11edb213290538316f24312e6
saradomi <115647886+saradomi@users.noreply.github.com>
2025-06-16 17:00:38 +0000

    Initial version

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as core from '@actions/core';
+
+
+
+try {
+    const name = core.getInput('name');
+    const output_value = `Hello $(name)!`;
+    core.setOutput('greeting', output_value );  
+} catch (error) {
+    core.setFailed(error.message)
+}
